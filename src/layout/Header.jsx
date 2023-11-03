cd81bf9b7f7ac690e878bc85b12eb910ef9cf425
nomanliaqat48 <noman.liaqat@invozone.com>
2023-11-03 21:20:21 +0500

    UI

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,18 +5,63 @@ import Round from "../assets/fluent_arrow-sync-checkmark-20-regular.svg";
 import Truck from "../assets/ph_truck-light.svg";
 import Heart from "../assets/mdi_cards-heart-outline.svg";
 import Tick from "../assets/fluent_checkmark-starburst-20-regular.svg";
+import NextIcon from "../assets/next.png";
+import PrevIcon from "../assets/prev.png";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Header.scss";
 
 const Header = () => {
+
+  function NextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <img
+      src={NextIcon}
+        className={className}
+        style={{ ...style }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function PrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <img
+      src={PrevIcon}
+        className={className}
+        style={{ ...style }}
+        onClick={onClick}
+      />
+    );
+  }
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <div className="header_Wrapper">
